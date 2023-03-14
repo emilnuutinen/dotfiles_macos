6602651ce1c1ca393d0682baa31257faecbaadee
Emil Nuutinen <emil@emilnuutinen.com>
2023-03-14 14:20:06 +0200

    Fix angle bracket autopairs to not pair on loop comparisons etc

--- .config/nvim/lua/extras.lua
+++ .config/nvim/lua/extras.lua
@@ -270,7 +270,9 @@ require("telescope").setup{
 
 -- Autopairs
 require("nvim-autopairs").setup {}
+local autopairs = require('nvim-autopairs')
 local Rule = require('nvim-autopairs.rule')
-local npairs = require('nvim-autopairs')
-npairs.add_rule(Rule("<",">",{"rust", "html"}))
-
+local cond = require("nvim-autopairs.conds")
+autopairs.add_rules {
+  Rule("<", ">"):with_pair(cond.before_regex("%a+")):with_move(),
+}
