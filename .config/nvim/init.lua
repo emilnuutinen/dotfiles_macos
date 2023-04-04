26028398b0b064aa92795cbc4aa47de7155ddf7d
Emil Nuutinen <emil@emilnuutinen.com>
2023-04-04 10:20:56 +0300

    Migrate from VimL to Lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,7 @@
+require ("extras")
+
+vim.cmd[[
+
 " Plugins
 call plug#begin(stdpath('data') . '/plugged')
 Plug 'preservim/vim-markdown'
@@ -17,55 +21,37 @@ Plug 'hrsh7th/nvim-cmp'
 " LSP completion source:
 Plug 'hrsh7th/cmp-nvim-lsp'
 
-" Useful completion sources:
+"-- Useful completion sources:
 Plug 'hrsh7th/cmp-nvim-lua'
 Plug 'hrsh7th/cmp-nvim-lsp-signature-help'
 Plug 'hrsh7th/cmp-vsnip'
 Plug 'hrsh7th/cmp-path'
 Plug 'hrsh7th/vim-vsnip'
 
-" Inlay hints
+"-- Inlay hints
 Plug 'lvimuser/lsp-inlayhints.nvim'
 
-" Themes
+"-- Themes
 Plug 'sainnhe/gruvbox-material'
 
 call plug#end()
 
 let $NVIM_TUI_ENABLE_TRUE_COLOR=1 
-set termguicolors
 
-set completeopt=menu,menuone,noselect,noinsert
-
-" Lua things
-lua require ("extras")
-
-let g:floaterm_keymap_toggle = '<F12>'
-let g:floaterm_autoclose = 2
-
-" Always show the signcolumn, otherwise it would shift the text each time
-" diagnostics appear/become resolved.
-if has("nvim-0.5.0") || has("patch-8.1.1564")
-  " Recently vim can merge signcolumn and number column into one
-  set signcolumn=number
-else
-  set signcolumn=yes
-endif
-
-" Find files using Telescope command-line sugar.
+"-- Find files using Telescope command-line sugar.
 nnoremap <leader>ff <cmd>Telescope find_files<cr>
 nnoremap <leader>fg <cmd>Telescope live_grep<cr>
 nnoremap <leader>fc <cmd>Telescope git_commits<cr>
 nnoremap <leader>fr <cmd>Telescope spell_suggest<cr>
 nnoremap <leader>fb <cmd>Telescope file_browser<cr>
 
-" More natural movement
+"-- More natural movement
 noremap ; l
 noremap l k
 noremap k j
 noremap j h
 
-" More natural window movement
+"-- More natural window movement
 nnoremap <C-h> <C-W>h
 nnoremap <C-j> <C-W>j
 nnoremap <C-k> <C-W>k
@@ -73,42 +59,30 @@ nnoremap <C-l> <C-W>l
 
 nnoremap <CR> :noh<CR><CR>
 
-" Map arrow keys to move naturally
+"-- Map arrow keys to move naturally
 imap <silent> <Down> <C-o>gj
 imap <silent> <Up> <C-o>gk
 nmap <silent> <Down> gj
 nmap <silent> <Up> gk
 
-" Enable use of the mouse for all modes
-set mouse=a
-
-" map leader to Space
+"-- map leader to Space
 nnoremap <SPACE> <Nop>
 map <SPACE> <Leader>
 
-" Disable cursor changing in insert mode
-set guicursor=
-
-" Use system clipboard
-set clipboard+=unnamedplus
-
-" Insert bolded text with a timestamp and subject in markdown
+"-- Insert bolded text with a timestamp and subject in markdown
 imap <silent> <F1> <C-R>=strftime("**[%H:%M]** ")<CR><C-o>:cal cursor(0,13)<CR>
 nmap <silent> <F1> i<C-R>=strftime("**[%H:%M]** ")<CR><C-o>:cal cursor(0,13)<CR>
 
-" Insert a new todo template
+"-- Insert a new todo template
 imap <silent> <F2> <C-R>="- [ ] "<CR><C-o>:cal cursor(0,10)<CR>
 nmap <silent> <F2> i<C-R>="- [ ] "<CR><C-o>:cal cursor(0,12)<CR>
 
-" Insert heading with todays date in markdown
+"-- Insert heading with todays date in markdown
 imap <silent> <F3> <C-R>=strftime("### %d-%m-%Y")<CR>
 nmap <silent> <F3> i<C-R>=strftime("### %d-%m-%Y")<CR>
 
-" 1 tab is 2 spaces
-set tabstop=2
-set softtabstop=0
-set shiftwidth=2
-set expandtab
+" Scroll past lastline
+set display+=lastline
 
 " Python tabbing (PEP 8)
 au BufNewFile,BufRead *.py set filetype=python 
@@ -128,49 +102,7 @@ autocmd filetype go setlocal shiftwidth=4
 autocmd filetype go setlocal expandtab
 
 
-" Use spaces instead of tabs
-set expandtab
-
-" Hide foldcolumn
-set foldcolumn=0
-highlight FoldColumn guibg=0
-" disable folding
-set nofoldenable
-
-" Colorcolumn
-set colorcolumn=80
-
-" Highlight current line number
-set cursorline
-set cursorlineopt=number
-
-" Turn backup off
-set nobackup
-set nowb
-set noswapfile
-
-set si "Smart indent
-set wrap "Wrap lines
-
-" Set utf8 as standard encoding
-set encoding=utf-8
-
-" Ignore casing of normal letters
-set ignorecase
-
-" Ignore casing when using lowercase letters only
-set smartcase
-
-set breakindent
-set linebreak
-
-" Scroll past lastline
-set display+=lastline
-
-" Show line nubmers
-set number
-
-" Set theme
+" vim.opt.theme  
 set background=dark
 let g:gruvbox_material_background='hard'
 let g:gruvbox_material_foreground='original'
@@ -243,21 +175,6 @@ set splitright
 set splitbelow
 nnoremap <Leader>=  <C-W>=
 
-" enable setting title
-set title
-
-" configure title only show filename and flags
-set titlestring=%t\ %r%m titlelen=50
-
-" Statusline colors
-hi User1 guibg=NONE guifg=#b8bb26
-hi User2 guibg=NONE guifg=#fabd2f
-hi User3 guibg=NONE guifg=#fb4934
-hi StatusLineTermNC guifg=NONE guibg=NONE
-
-" Always display the status line, even if only one window is displayed
-set laststatus=3
-
 " Normal statusline
 set statusline=%1*                  " change colors
 set statusline+=[%{GetMode()}]      " show mode
@@ -318,27 +235,109 @@ autocmd filetype markdown set nolist
 " Fix wrapping lists with gq
 autocmd FileType markdown set comments=fb:*,fb:+,fb:-,n:> indentexpr=
 
-" Config for vim-markdown
-let g:vim_markdown_folding_disabled = 1
-let g:vim_markdown_conceal = 0
-let g:vim_markdown_conceal_code_blocks = 0
-let g:vim_markdown_math = 1
-let g:vim_markdown_frontmatter = 1
-let g:vim_markdown_strikethrough = 1
-let g:vim_markdown_autowrite = 1
-let g:vim_markdown_edit_url_in = 'tab'
-let g:vim_markdown_new_list_item_indent = 0
-let g:vim_markdown_auto_insert_bullets = 0
-let g:tex_conceal = ""
-let g:vim_markdown_math = 1
-let g:vim_markdown_frontmatter = 1
+" Use system clipboard
+set clipboard+=unnamedplus
 
-" File explorer
-let g:netrw_banner=0
-let g:netrw_liststyle=3
-let g:netrw_browse_split=4
-let g:netrw_altv=1
-let g:netrw_winsize=20
+highlight FoldColumn guibg=0
+
+" Disable cursor changing in insert mode
+set guicursor=
+
+" configure title only show filename and flags
+set titlestring=%t\ %r%m titlelen=50
+
+" Statusline colors
+hi User1 guibg=NONE guifg=#b8bb26
+hi User2 guibg=NONE guifg=#fabd2f
+hi User3 guibg=NONE guifg=#fb4934
+hi StatusLineTermNC guifg=NONE guibg=NONE
 
 " Format on save
 autocmd BufWritePre * lua vim.lsp.buf.format()
+
+" Hide foldcolumn
+set foldcolumn=0
+]]
+
+vim.g.floaterm_keymap_toggle = '<F12>'
+vim.g.floaterm_autoclose = 2
+
+vim.opt.termguicolors = true
+vim.opt.completeopt = 'menu,menuone,noselect,noinsert'
+
+vim.opt.signcolumn = 'number'
+
+-- Enable use of the mouse for all modes
+vim.opt.mouse = 'a'
+
+
+-- 1 tab is 2 spaces
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 0
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+
+
+-- disable folding
+vim.opt.foldenable = false
+
+-- Colorcolumn
+vim.opt.colorcolumn = {80}
+
+-- Highlight current line number
+vim.opt.cursorline = true
+vim.opt.cursorlineopt = 'number'
+
+-- Turn backup off
+vim.opt.backup = false
+vim.opt.wb = false
+vim.opt.swapfile = false
+
+vim.opt.si = true
+vim.opt.wrap = true
+
+-- vim.opt.utf8 as standard encoding
+vim.opt.encoding = 'utf-8'
+
+-- Ignore casing of normal letters
+vim.opt.ignorecase = true
+
+-- Ignore casing when using lowercase letters only
+vim.opt.smartcase = true
+
+vim.opt.breakindent = true
+vim.opt.linebreak = true
+
+-- Show line nubmers
+vim.opt.number = true
+
+-- enable setting title
+vim.opt.title = true
+
+
+-- Always display the status line, even if only one window is displayed
+vim.opt.laststatus = 3
+
+
+-- Config for vim-markdown
+vim.g.vim_markdown_folding_disabled = 1
+vim.g.vim_markdown_conceal = 0
+vim.g.vim_markdown_conceal_code_blocks = 0
+vim.g.vim_markdown_math = 1
+vim.g.vim_markdown_frontmatter = 1
+vim.g.vim_markdown_strikethrough = 1
+vim.g.vim_markdown_autowrite = 1
+vim.g.vim_markdown_edit_url_in = 'tab'
+vim.g.vim_markdown_new_list_item_indent = 0
+vim.g.vim_markdown_auto_insert_bullets = 0
+vim.g.tex_conceal = ""
+vim.g.vim_markdown_math = 1
+vim.g.vim_markdown_frontmatter = 1
+
+-- File explorer
+vim.g.netrw_banner = 0
+vim.g.netrw_liststyle = 3
+vim.g.netrw_browse_split = 4
+vim.g.netrw_altv = 1
+vim.g.netrw_winsize = 20
+
