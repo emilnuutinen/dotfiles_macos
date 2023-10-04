9eaaf7a03b36b34af240906e7e33ebf174909c97
Emil Nuutinen <emil@emilnuutinen.com>
2023-10-04 12:35:38 +0300

    Open dev learning journal on the side with weekly journal

--- .scripts/tmux/base.sh
+++ .scripts/tmux/base.sh
@@ -2,11 +2,14 @@
 
 session="base"
 
+# current week as filename
+journal=journal_w$(gdate "+%V").md
+
 tmux new-session -d -s $session
 
 window=1
 tmux rename-window -t $session:$window 'note'
-tmux send-keys -t $session:$window 'jf' C-m
+tmux send-keys -t $session:$window 'cd notes && vim -O personal/journal/2023/'$journal' dev/rust/06_change_in_course.md' C-m
 
 window=2
 tmux new-window -t $session:$window
